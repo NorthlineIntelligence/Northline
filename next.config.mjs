f4fd24e11627cd5a9e6e0d811a042d2360dd1deb
Josh Decker <Josh@northlineintelligence.com>
2026-03-05 19:36:03 -0600

    Fix next config keys for Vercel tracing

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Force Next to generate the *.nft.json tracing files Vercel expects
-    outputFileTracing: true,
-  
-    experimental: {
-      // Helps Vercel tracing resolve paths correctly
-      outputFileTracingRoot: process.cwd(),
-    },
+    outputFileTracingRoot: process.cwd(),
   };
   
   export default nextConfig;
+  
